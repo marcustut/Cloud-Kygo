f79130b0d3e601ec95f609d500a45dd099e0df29
Marcus Lee <marcustutorial@hotmail.com>
2020-07-22 03:37:24 +0800

    Configure Cloud Firestore and added Suggestion feature

--- main.js
+++ main.js
@@ -1,21 +1,37 @@
+// Modules for Discord
 const { Client, MessageEmbed }= require('discord.js');
 const main = new Client;
 
+// Firebase Config
+const firebase = require('firebase-admin');
+const { firebaseConfig } = require('./firebase-config.json');
+const serviceAccount = require('./serviceAccount.json');
+
+firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount)
+});
+
+const db = firebase.firestore();
+
+// 3rd Party Modules
 const cheerio = require('cheerio');
 const request = require('request');
 
-const PREFIX = process.env.prefix;
+// Self defined modules
+const announce = require('./commands/announce')
+const poll = require('./commands/poll')
+const event = require('./commands/event')
 
-var announce = require('./commands/announce')
-var poll = require('./commands/poll')
-var event = require('./commands/event')
+// Getting Environment Variables
+const PREFIX = process.env.d_PREFIX;
+const discordToken = process.env.d_TOKEN;
 
 main.once('ready', () =>{
     console.log("KYGO ARE ONLINE!");
 });
 
-main.on('message', message =>{
-    let args = message.content.substring(PREFIX.length).split(" ");
+main.on('message', async message =>{
+    let args = message.content.substring(PREFIX.length).trim().split(" ");
     const command = args.shift().toLowerCase();
 
     if(command === 'announce'){
@@ -107,6 +123,61 @@ main.on('message', message =>{
             })
         }
     }
+    if (message.content.startsWith('$suggestion')) {
+        // Parsing data from template message
+        const parsedMesssage = message.content.trim().split('\n');
+        const user = {
+            IGN: parsedMesssage[1].split(':')[1].trim(),
+            suggestion: parsedMesssage[2].split(':')[1].trim(),
+            reason: parsedMesssage[3].split(':')[1].trim()
+        };
+
+        try {
+            // Specifying the collections
+            const commandsCollection = db.doc('/discord/commands');
+            const commandsDoc = await commandsCollection.get();
+            const suggestionCollection = db.collection('/discord/commands/suggestions').doc(`suggestion${commandsDoc.data().suggestionsCount + 1}`);
+
+            // Creating a new document
+            await suggestionCollection.set({
+                'id': commandsDoc.data().suggestionsCount + 1,
+                'username': message.author.username,
+                'userID': message.author.id,
+                'userAvatar': message.author.displayAvatarURL(),
+                'IGN': user.IGN,
+                'date': new Date(),
+                'suggestion': user.suggestion,
+                'reason': user.reason,
+                'response': null
+            });
+
+            // Creating a Embed reply to user
+            const suggestionEmbed = new MessageEmbed()
+                .setAuthor(message.author.tag, message.author.displayAvatarURL())
+                .setTitle(`Suggestion #${commandsDoc.data().suggestionsCount + 1}`)
+                .setDescription(`IGN: ${user.IGN}\nSuggestion: ${user.suggestion}\nReason: ${user.reason}`)
+                .setFooter(main.user.username, main.user.displayAvatarURL())
+                .setTimestamp();
+
+            message.channel.send(suggestionEmbed).then(embedMessage => {
+                embedMessage.react("⬆️");
+                embedMessage.react("⬇️");
+            });
+
+            // Logging the result
+            console.log(`Suggestion#${commandsDoc.data().suggestionsCount} by ${message.author.tag} at ${new Intl.DateTimeFormat('en-US', { year: 'numeric', month: '2-digit', day: '2-digit', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZone: 'Asia/Kuala_Lumpur', timeZoneName: 'short'}).format(new Date())} (A new suggestion is issued)`)
+
+            // Increment suggestionsCount 
+            commandsCollection.update({
+                'suggestionsCount': commandsDoc.data().suggestionsCount + 1
+            });
+
+            return;
+        } catch (error) {
+            console.log(error.message);
+            return message.channel.send('An Error Occured\nMake sure your template is correct.');
+        }
+    }
 });
 
-main.login(process.env.token);
+main.login(discordToken);
